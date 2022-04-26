cc29bf33ad306f2bb45ba5dccd327fbfffff1895
Gonzalo <jogozarateri@ittepic.edu.mx>
2022-04-26 17:18:39 -0600

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  
   constructor() {}
 
+  insert(){    
+    var div = document.getElementById("contenedor")
+    div.innerHTML = "<ion-label (click)='insert2()' style='color:black;'>Insertar elementos 5</ion-label>"
+  }
+
+  insert2(){
+    var div = document.getElementById("contenedor")
+    div.innerHTML = "<ion-button (click)='insert()' style='color:black;'>Insertar elementos</ion-button>"
+  }
 }
