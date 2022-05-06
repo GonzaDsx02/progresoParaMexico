ddad291b6af53c8a53322acace046d65022ac215
Gonzalo <jogozarateri@ittepic.edu.mx>
2022-05-05 21:43:56 -0600

    report page elements are now validated

--- src/app/pages/report/report.module.ts
+++ src/app/pages/report/report.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +13,8 @@ import { ReportPage } from './report.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    ReportPageRoutingModule
+    ReportPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [ReportPage]
 })
--- src/app/pages/report/report.page.ts
+++ src/app/pages/report/report.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-report',
@@ -7,6 +7,9 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./report.page.scss'],
 })
 export class ReportPage implements OnInit {
+
+  public formLogin: FormGroup;
+  public validationMessages: object;
 
   violence_type: string;
   victim_gen: string;
@@ -16,9 +19,51 @@ export class ReportPage implements OnInit {
   victim_role: string;
   aggressor_role: string;
   school: string;
-  school_place: string;
+  school_place: string;  
 
-  constructor() { 
+  constructor(private fb: FormBuilder) { 
+    this.formLogin = this.fb.group({
+      v_type: ['', Validators.required],
+      vic_gen: ['', Validators.required],
+      ag_gen:['', Validators.required],
+      ag_name: ['', Validators.required],
+      place_form: ['', Validators.required],
+      vic_role: ['', Validators.required],
+      ag_role: ['', Validators.required],
+      school_form: ['', Validators.required],
+      sc_place_form: ['', Validators.required]
+    });
+
+    this.validationMessages = {
+      v_type: [
+        { type: 'required', message: "Obligatorio!" }
+      ],      
+      vic_gen: [
+        { type: 'required', message: "Obligatorio!" }
+      ],      
+      ag_gen: [
+        { type: 'required', message: "Obligatorio!" }
+      ],
+      ag_name: [
+        { type: 'required', message: "Obligatorio!" }
+      ],
+      place_form: [
+        { type: 'required', message: "Obligatorio!" }
+      ],
+      vic_role: [
+        { type: 'required', message: "Obligatorio!" }
+      ],
+      ag_role: [
+        { type: 'required', message: "Obligatorio!" }
+      ],
+      school_form: [
+        { type: 'required', message: "Obligatorio!" }
+      ],
+      sc_place_form: [
+        { type: 'required', message: "Obligatorio!" }
+      ]
+    }
+
     this.violence_type = "";
     this.victim_gen = ""
     this.aggressor_gen = "";
@@ -32,9 +77,10 @@ export class ReportPage implements OnInit {
   ngOnInit() {
   }
 
-  send(form: NgForm){
+  send(){
     //Aqui van los codigos que envían el formulario a la base de datos
-    alert("Datos enviados")
+    alert(this.victim_gen);
   }
 
 }
+
