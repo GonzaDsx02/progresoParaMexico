f38aa112d2cc3428a167463218ef44044dc83105
DamarisGallegos <90273417+DamarisGallegos@users.noreply.github.com>
2022-05-11 23:00:56 -0600

    Revert "Merge branch 'master' of https://github.com/GonzaDsx02/progresoParaMexico"
    This reverts commit 2e5a9e4b25eccdb3d11c97875ea2ea3706d555ba, reversing
    changes made to 33c58f8c9553443b6ae84834081dbef3e718fa51.

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
+import { Alert } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-login',
@@ -8,23 +11,21 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginPage implements OnInit {
 
-  /**
-  * Variables para la validación del formulario de login (No alterar)
-  * @var formLogin Formulario para iniciar sesión
-  * @var validationMessages Mensajes de validación
-  */
+  //variables para la validacion del formulario. **** No tocar plis ****
   public formLogin: FormGroup;
   public validationMessages: object;
 
   username: string;
   password: string;
-  /**
-   * Constructor de la clase
-   * @param formBuilder para crear el formulario
-   * @param validationMessages para crear el mensaje de validacion (Para requerir el campo de usuario y contraseña)
-   */
-  constructor(private fb: FormBuilder) { 
 
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private router : Router
+    ) { 
+
+      
+    
     this.formLogin = this.fb.group({
       us_name: ['', Validators.required],
       pass: ['', Validators.required]
@@ -46,11 +47,27 @@ export class LoginPage implements OnInit {
   ngOnInit() {
   }
 
-  /**
-   * Funcion para inicio de sesion
-  */
   login() {
     //Aqui va todo el guateque
+   this.userService.loginUser(this.username,this.password)
+   .then(response=>{
+     console.log(response);
+     this.router.navigate(['/panel-admin']);
+   })
+   .catch(error => {
+     console.log(error),
+     alert('Error. Datos incorrectos');
+    });
+    
   }
+
+  /*registro() {
+    this.userService.register(this.formLogin.value)
+    .then(response=>{
+      console.log(response);
+    })
+    .catch(error=>console.log(error));
+    
+  }*/
 
 }
