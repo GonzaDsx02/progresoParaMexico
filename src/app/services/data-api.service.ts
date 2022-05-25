fa763cb17b0a32bcf239f86c5a4c24f7131438a0
charlie1525 <caurfregosoes@ittepic.edu.mx>
2022-05-25 11:28:35 -0600

    Añadiendo la documentacion en el servicio data-api.service.ts

--- src/app/services/data-api.service.ts
+++ src/app/services/data-api.service.ts
@@ -13,14 +13,19 @@ export class DataApiService {
 
   constructor(private afs: AngularFirestore) {
     this.itemCollection = afs.collection<Item>('Reports');
-    this.itemCollectionS = afs.collection('EscuelasSuperior')
-    this.item = this.itemCollection.valueChanges()
+    this.itemCollectionS = afs.collection('EscuelasSuperior');
+    this.item = this.itemCollection.valueChanges();
+    this.itemS = this.itemCollectionS.valueChanges();
    }
 
   private itemCollection: AngularFirestoreCollection<Item>;
   private itemCollectionS:AngularFirestoreCollection;
   private itemS: Observable<any[]>;
   private item: Observable<Item[]>
+
+  /**
+   * getAll2() - Segunda prueba para la recuperacion general de la informacion dentro de firestore
+   */
 
   getAll2(){
     return this.item = this.itemCollection.snapshotChanges()
@@ -33,6 +38,9 @@ export class DataApiService {
     )));
   };//fin del segundo método para tener todos los datos
 
+  /**
+   * getAggressors() - Método que regresa el genero de los agresores en todos los reportes dentro de Firestore
+   */
   getAggressors(){
     return this.itemCollection.snapshotChanges().pipe(
       map(
@@ -46,7 +54,9 @@ export class DataApiService {
     )// fin del pipe
   }// fin del metodo Agresores
 
-
+/**
+ * getEscuelas() - Método encargado de recuperar el nivel educativo de las esucelas, teniendo solo 2 posibles reultados, Medio Superior o Superior
+ */
   getEscuelas() {
     return this.itemCollection.snapshotChanges().pipe(
       map(
@@ -60,7 +70,10 @@ export class DataApiService {
     )// fin del pipe
   }// fin del metodo escuelas
 
-
+/**
+ *  getViolenceType() - Método encargado de recuperar el tipo de violencia en los reportes
+* @returns El tipo de vilencia registrada
+ */
   getViolenceType() {
     return this.itemCollection.snapshotChanges().pipe(
       map(
@@ -74,6 +87,10 @@ export class DataApiService {
     )// fin del pipe
   };// fin del metodo escuelas
 
+  /**
+   *  getEscuelaName() - Método encargado de recupear el nombre de las escuelas
+   * @returns nombreEscuela: Nombre de la escuela
+   */
   getEscuelasName(){
     return this.itemCollectionS.snapshotChanges().pipe(
       map(action=> action.map(
@@ -85,6 +102,10 @@ export class DataApiService {
     )//fin del pipe
   };
 
+  /**
+   * getProfes() - Método encargado de recuperar el nombre de los agresores
+   * @returns profe: Nombre del professor, variable marcada como nombre del agresor
+   */
   getProfes(){
     return this.itemCollection.snapshotChanges().pipe(
       map(action => action.map(
